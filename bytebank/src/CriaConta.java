53d371abe24ad84e723ce38ae9623058567707aa
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-24 17:35:51 -0300

    criando os atributos da conta

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -5,5 +5,17 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("Primeira conta tem "+ primeiraConta.saldo);
+		System.out.println("Segunda conta tem "+ segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
 	}
 }
