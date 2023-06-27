81895412263da0c48e360b2f4c94dff6e36175ba
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-27 19:53:23 -0300

    Métodos com referencia e mais retorno

--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -8,5 +8,16 @@ public class TestaMetodo {
 		boolean conseguiuRetirar = contaDoPaulo.saca(20);
 		System.out.println(contaDoPaulo.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaPatrica = new Conta();
+		contaDaPatrica.deposita(1000);
+		
+		if(contaDaPatrica.transfere(300, contaDoPaulo)) {
+			System.out.println("Tranferência feita com sucesso!!");
+		}else {
+			System.out.println("saldo insuficiente para tranferência!");
+		}
+		System.out.println(contaDaPatrica.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
 }
