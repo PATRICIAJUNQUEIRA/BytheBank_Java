f65220e24e5d55552fb10e8c7bd9b80f7a29e3c9
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-27 19:10:19 -0300

    Métodos com retornos

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -3,4 +3,19 @@ class Conta{
 	int agencia;
 	int numero;
 	String titular;
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}else {
+			return false;
+		}
+	}
+
 }
+
