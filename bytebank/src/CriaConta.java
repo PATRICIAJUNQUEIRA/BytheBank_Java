8bcaf9e662b43f1af7b90e72e2c2dc643721d798
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-06-27 14:26:04 -0300

    Definindo as referencias na Orientação Objetos

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -1,10 +1,9 @@
-
 public class CriaConta {
 	
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
-		System.out.println(primeiraConta);
+		System.out.println(primeiraConta.saldo);
 		
 		primeiraConta.saldo += 100;
 		System.out.println(primeiraConta.saldo);
@@ -15,7 +14,20 @@ public class CriaConta {
 		System.out.println("Primeira conta tem "+ primeiraConta.saldo);
 		System.out.println("Segunda conta tem "+ segundaConta.saldo);
 		
+		primeiraConta.agencia = 146;
 		System.out.println(primeiraConta.agencia);
 		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia = 146;
+		System.out.println("Agora a segunda conta esta"
+				+ "na agencia"+ segundaConta.agencia);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("mesma conta");
+		}else {
+			System.out.println("contas diferentes");
+		}
 	}
 }
