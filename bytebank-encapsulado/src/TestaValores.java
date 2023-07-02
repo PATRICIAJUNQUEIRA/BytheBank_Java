6bc9f7b7b73a7131dbccf0a7468f53da4aa8acd9
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-07-01 21:44:13 -0300

    Static

--- bytebank-encapsulado/src/TestaValores.java
+++ bytebank-encapsulado/src/TestaValores.java
@@ -5,5 +5,12 @@ public class TestaValores {
 		Conta conta = new Conta(1337, 24226);
 		
 		System.out.println(conta.getAgencia());
+		
+		conta.setAgencia(1234568);
+		
+		Conta conta2 = new Conta(1337, 16549);
+		Conta conta3 = new Conta(13468, 42358);
+		
+		System.out.println(Conta.getTotal());
 	}
 }
